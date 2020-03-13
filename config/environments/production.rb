008a78e21105a8cc69e96673f23a9583f66ac028
Lemeri02 <9588504@gmail.com>
2020-03-13 23:58:25 +0500

    fix locales bug on ru.yml & fix port for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,14 +113,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  
+
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
