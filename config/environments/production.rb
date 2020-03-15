c93b00479001aad4c9d9d9f1f5a3296e96e29a58
Lemeri02 <9588504@gmail.com>
2020-03-15 18:14:41 +0500

    email notification for photo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,16 +114,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
 
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com',
-  #   :enable_starttls_auto => true
-  # }
-
     ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
