048a121284345508a4fe5d739d4cb7d28ecc4582
Lemeri02 <9588504@gmail.com>
2020-03-17 00:07:03 +0500

    fix local timezone, refactoring email layouts

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,16 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'dev9588504@gmail.com'
 
   layout 'mailer'
+
+  helper_method :time_locale_to
+
+  private
+
+  def time_locale_to(event)
+    if event.datetime.localtime > Time.now
+      "#{t('event_mailer.due_datetime')}: #{l(@event.datetime.localtime, format: :short)}"
+    else
+      "#{t('event_mailer.exp_datetime')}: #{l(@event.datetime.localtime, format: :short)}"
+    end
+  end
 end
