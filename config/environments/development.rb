39d3b41eaf9f2e70defb5648c533fc69ec850a02
Lemeri02 <9588504@gmail.com>
2020-04-23 17:06:49 +0500

    Activejob - dev settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
-  config.active_job.queue_adapter = :resque
+  config.active_job.queue_adapter = :inline
   config.active_job.queue_name_prefix = "bbq_#{Rails.env}"
 end
