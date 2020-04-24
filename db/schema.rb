7d77f8dd685d63b89d948d398dd0bc689dd665a3
Lemeri02 <9588504@gmail.com>
2020-04-24 20:57:43 +0500

    delete field when email null false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_163253) do
+ActiveRecord::Schema.define(version: 2020_04_24_101235) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_163253) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "encrypted_password", default: "", null: false
