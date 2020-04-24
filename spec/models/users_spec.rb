2df3d29f2eed22a089021cbf52b8206a24766cdb
Lemeri02 <9588504@gmail.com>
2020-04-25 01:49:44 +0500

    add tests for vk omniauth

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe User, type: :model do
         create(:user, provider: 'facebook',
                url: 'https://facebook.com/2865974682365812')
       end
-      let!(:some_other_uer) { create(:user) }
+      let!(:some_other_user) { create(:user) }
 
       it 'returns this user' do
         expect(User.find_for_facebook_oauth(access_token)).to eq existing_user
@@ -67,13 +67,19 @@ RSpec.describe User, type: :model do
     end
 
     context 'when users profile on social network dont have email' do
-      let!(:existing_user) { create(:user, email: '') }
+      let!(:access_token) do
+        double(
+          :access_token,
+          provider: 'vkontakte',
+          info: double(email: nil, name: 'Vasya Vkontaktin'),
+          extra: double(raw_info: double(id: '333666888'))
+        )
+      end
       it 'returns newly created user' do
         user = User.find_for_vkontakte_oauth(access_token)
 
         expect(user).to be_persisted
-        expect(user.email).to eq 'vasya@mail.com'
-        byebug
+        expect(user.email).to eq '333666888@vkontakte'
       end
     end
 
@@ -89,9 +95,9 @@ RSpec.describe User, type: :model do
     context 'when user is found by provider + url' do
       let!(:existing_user) do
         create(:user, provider: 'vkontakte',
-               url: 'https://vk.com/333666888')
+               url: 'https://vk.com/id333666888')
       end
-      let!(:some_other_uer) { create(:user) }
+      let!(:some_other_user) { create(:user) }
 
       it 'returns this user' do
         expect(User.find_for_vkontakte_oauth(access_token)).to eq existing_user
