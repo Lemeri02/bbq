15dafd395c1a7c0798759658799aaad2d444a8fa
Lemeri02 <9588504@gmail.com>
2020-04-27 02:50:31 +0500

    create new field to user for remote avatar url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_165446) do
+ActiveRecord::Schema.define(version: 2020_04_26_213503) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_165446) do
     t.string "avatar"
     t.string "provider"
     t.string "url"
+    t.string "remote_avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
